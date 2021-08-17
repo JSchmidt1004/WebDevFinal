5a50e31bc7693cfa2f3d38943f1120fd003f9057
JSchmidt1004 <67611141+JSchmidt1004@users.noreply.github.com>
2021-08-17 08:29:11 -0600

    Basic database connection
    We still need to save and the like, but I'm not messing with that until we have pages to save the answers

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,32 @@
+const mongoose = require("mongoose");
 
+mongoose.Promise = global.Promise;
+
+mongoose.connect("mongodb+srv://GKilpack:Kilpack1@cluster0.99tyg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+{
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+});
+
+mongoose.set("useCreateIndex", true);
+mongoose.set("useFindAndModify", false);
+
+let mdb = mongoose.connection;
+mdb.on("error", console.error.bind(console, "connection error"));
+mdb.once("open",  callback => {});
+
+let userSchema = mongoose.Schema(
+    {
+        username: String,
+        password: String,
+        email: String,
+        age: String,
+        answer1: String,
+        answer2: String,
+        answer3: String
+    });
+
+let User = mongoose.model("User_Collection", userSchema);
 
 exports.index = (request, response) =>
 {
