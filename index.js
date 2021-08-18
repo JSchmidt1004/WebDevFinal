b3c784c70aba562408e5191074ee57c5100eaf11
Garrick Kilpack <garrickkilpack@hotmail.com>
2021-08-18 07:30:06 -0600

    Account Creation alpha
    Created basic pug files to at least test account creation, made create and createAccount methods in routes.js.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const pug = require("pug");
+const routes = require('./routes/routes');
+const path = require('path');
 const cookieParser = require("cookie-parser");
 
 const app = express();
 
+app.set('view engine' , 'pug');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, '/public')));
+
+let urlencodedParser  = express.urlencoded({
+    extended: false
+});
+
 app.get("/", routes.index);
+
+app.get('/create', routes.create);
+app.post('/create', urlencodedParser, routes.createAccount);
 
 app.listen(3000);
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 mongoose.Promise = global.Promise;
 
@@ -28,7 +29,48 @@ let userSchema = mongoose.Schema(
 
 let User = mongoose.model("User_Collection", userSchema);
 
+/*
 exports.index = (request, response) =>
 {
     response.send("This is index");
 }
+*/
+
+exports.index = (req, res) => {
+    User.find((err, user) => {
+        if(err) return console.error(err);
+        res.render('index', {
+            title: 'User List',
+            users: user
+        });
+    });
+};
+
+exports.create = (req, res) => {
+    res.render('create', {
+        title: 'Create Account'
+    });
+};
+
+exports.createAccount = (req, res) => {
+    let preHashPassword = req.body.password;
+    // Do Hashing for Password
+    let postHashPassword = preHashPassword;
+
+    let user = new User({
+        username: req.body.username,
+        password: postHashPassword,
+        email: req.body.email,
+        age: req.body.age,
+        answer1: req.body.answer1,
+        answer2: req.body.answer2,
+        answer3: req.body.answer3
+    });
+
+    user.save((err, user) => {
+        if (err) return console.error(err);
+        console.log(`${user.username}'s account added`);
+    });
+
+    res.redirect('/');
+};
