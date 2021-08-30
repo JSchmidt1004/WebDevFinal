b29b76fdf774e73437dd98b9d5f92f95a732b281
JSchmidt1004 <67611141+JSchmidt1004@users.noreply.github.com>
2021-08-30 08:53:12 -0600

    Basic setup for graph
    I need to get the data in there, but this is working so far. It's in an odd position and colors aren't working. No clue why

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,39 @@
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("bargraph");
 const ctx = canvas.getContext("2d");
 
 let url = "http://localhost:3000/api";
 
+canvas.width = 800;
+canvas.height = 600;
+
+const graphSetup = () =>
+{
+    //Outlining the canvas because the colors aren't working
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+    //Showing the numbers
+    let lineHeight = 0;
+    for(let i = (canvas.height / 50) - 1; i > 0; i--)
+    {
+        ctx.fillText(i, 5, lineHeight, 20);
+
+        //Lines for cleanliness
+        ctx.beginPath();
+        ctx.moveTo(20, lineHeight);
+        ctx.lineTo(canvas.width, lineHeight);
+        ctx.stroke();
+
+        lineHeight += 50;
+    }
+}
+
 const main = data =>
 {
     console.log(data);
+    graphSetup();
+
+    let question1 = data[1];
+    console.log(question1.question);
 };
 
 fetch(url)
