2163f4554ce77aecba9920ecc0f3cdc431471747
JSchmidt1004 <67611141+JSchmidt1004@users.noreply.github.com>
2021-08-31 08:40:41 -0600

    Graph should be done
    Doesn't look the best, but it's done

--- frontend/script.js
+++ frontend/script.js
@@ -6,6 +6,9 @@ let url = "http://localhost:3000/api";
 canvas.width = 800;
 canvas.height = 600;
 
+let startPoint = 0;
+let index = 0;
+
 const graphSetup = () =>
 {
     //Outlining the canvas because the colors aren't working
@@ -13,9 +16,9 @@ const graphSetup = () =>
 
     //Showing the numbers
     let lineHeight = 0;
-    for(let i = (canvas.height / 50) - 1; i > 0; i--)
+    for(let i = (canvas.height / 50); i > 0; i--)
     {
-        ctx.fillText(i, 5, lineHeight, 20);
+        ctx.fillText(i - 1, 5, lineHeight, 20);
 
         //Lines for cleanliness
         ctx.beginPath();
@@ -23,22 +26,59 @@ const graphSetup = () =>
         ctx.lineTo(canvas.width, lineHeight);
         ctx.stroke();
 
+        if (i == 1) startPoint = lineHeight;
         lineHeight += 50;
+    }
+}
+
+const fillGraph = questionData =>
+{
+    questionData.question;
+    ctx.fillStyle = "#111";
+    ctx.fillText(questionData.question, (canvas.width / 2) - 50, 25, 250);
+
+    let options = [questionData.option1, questionData.option2, questionData.option3, questionData.option4];
+    let counts = [questionData.option1Count, questionData.option2Count, questionData.option3Count, questionData.option4Count];
+
+    let x = 100;
+    let y = 0;
+    let width = 125;
+    let height = 0;
+    let distanceBetween = 75;
+
+    for(let i = 0; i < 4; i++)
+    {
+        ctx.fillText(options[i], x - 10, canvas.height - 15, width);
+
+        let currentCount = 50 * counts[i];
+        ctx.fillRect(x - (width / 2), startPoint - currentCount, width, currentCount);
+
+        x += width + distanceBetween;
     }
 }
 
 const main = data =>
 {
-    console.log(data);
+    ctx.font = "25px Arial";
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     graphSetup();
-
-    let question1 = data[1];
-    console.log(question1.question);
+    fillGraph(data[index % data.length]);
 };
 
-fetch(url)
+
+const loop = () =>
+{
+    fetch(url)
     .then(response => response.json())
     .then(data => 
         {
             main(data);
         });
+
+    index++;
+}
+
+loop();
+
+setInterval(loop, 15000);
